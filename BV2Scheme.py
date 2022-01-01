b95ad360b3306dfc1a9cc34c29a71a310c17c719
Kelejun <63855342+Kelejun@users.noreply.github.com>
2022-01-01 13:00:51 +0800

    Add files via upload

--- BV2Scheme.py
+++ BV2Scheme.py
@@ -2,16 +2,16 @@
 from json.decoder import JSONDecodeError
 import requests
 
-BV2AV_API = 'https://api.bilibili.com/x/web-interface/view'  # ?bvid=
+BVAPI = 'https://api.bilibili.com/x/web-interface/view'  # ?bvid=
 HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62'}
 
 def bv2av(bv):
-    r = requests.get(BV2AV_API, {'bvid': bv}, headers=HEADER)
+    r = requests.get(BVAPI, {'bvid': bv}, headers=HEADER)
     response = decode_json(r)
     try:
         return str(response['data']['aid'])
     except (KeyError, TypeError):
-        return '获取av号失败'
+        return '获取失败'
 
 def decode_json(r):
     try:
@@ -23,8 +23,8 @@ def decode_json(r):
 
 def main():
     bv = input('请输入BV号:')
-    print('对应 Scheme URI   bilibili://video/' + bv2av(bv))
-    input('按回车键退出')
+    print('对应 Scheme URI 为： bilibili://video/' + bv2av(bv))
+    input('按Enter退出')
 
 if __name__ == '__main__':
     main()
